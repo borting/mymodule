ef943ea3ebbca151dd8270742437b81effb95dc7
Borting Chen <bortingchen@gmail.com>
2016-06-28 14:39:35 +0800

    Add test file
    Add test/ and write a c++ test file.

---
+++ test/basic.cpp
@@ -0,0 +1,16 @@
+#include "mbed-drivers/mbed.h"
+#include "mymodule/module.h"
+
+void app_start(int argc, char** argv) {
+  myModulePrint("Hello World");
+
+  return;
+}
+
+#ifndef YOTTA_CFG_MBED_OS
+int main(int argc, char* argv[]) {
+  app_start(0. (char**) 0);
+
+  return 0;
+}
+#endif
